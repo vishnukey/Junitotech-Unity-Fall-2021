041f0c051f5decc7b87201b3684f46f06a9d0b9c
Riley Baker <vishnukey@gmail.com>
2021-10-02 13:00:53 -0600

    PlayerController Rigidbody interactions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,8 +15,10 @@ public class PlayerController : MonoBehaviour
      */
 
     private CharacterController _controller;
+    private Vector3 direction;
 
     public float speed;
+    public float mass;
     
     // Start is called before the first frame update
     void Start()
@@ -28,7 +31,12 @@ public class PlayerController : MonoBehaviour
     {
         float left_and_right = Input.GetAxis("Horizontal");
         float forward_and_back = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(left_and_right, 0,forward_and_back).normalized * speed;
+        direction = new Vector3(left_and_right, 0,forward_and_back).normalized * speed;
         _controller.SimpleMove(direction);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        hit.rigidbody?.AddForce(direction * speed * mass);
     }
 }
