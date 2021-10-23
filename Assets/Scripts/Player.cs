d37b6ca39a6f46aa3a9060cbec21fe6972954f8b
Riley Baker <vishnukey@gmail.com>
2021-10-23 16:16:34 -0600

    class 5, oct 23 scene updates

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class Player : MonoBehaviour
     public int maxHealth;
     
     private float _health;
+    private PlayerInput _input;
 
     // Start is called before the first frame update
     void Start()
     {
         _health = maxHealth;
         healthText.text = $"health: {_health}";
+        _input = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
@@ -27,5 +30,15 @@ public class Player : MonoBehaviour
     {
         _health -= amnt;
         healthText.text = $"health: {_health}";
+    }
+
+    public void OnThrow(/*InputValue value*/)
+    {
+        Debug.Log($"THROW");
+    }
+
+    public void OnPunch(/*InputValue value*/)
+    {
+        Debug.Log($"PUNCH");
     }
 }
