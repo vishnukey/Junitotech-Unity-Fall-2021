defb081df2ee1023927def51a7c2acff2aab5638
Riley Baker <vishnukey@gmail.com>
2021-10-23 15:50:53 -0600

    class 5, oct 23

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth;
+
+    private float _health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float amnt)
+    {
+        _health -= amnt;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+    public int maxHealth;
+    
+    private float _health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = maxHealth;
+        healthText.text = $"health: {_health}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float amnt)
+    {
+        _health -= amnt;
+        healthText.text = $"health: {_health}";
+    }
+}
