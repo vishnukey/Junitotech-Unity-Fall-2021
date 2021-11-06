85c09c35506d7e5d60528b8a90cb133867fed0d2
Riley Baker <vishnukey@gmail.com>
2021-11-06 13:26:23 -0600

    class 7, november 6, partial - added a floating coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Coin : MonoBehaviour
+{
+
+    public float bobOffset;
+    [FormerlySerializedAs("bobSpeed")] public float bobPeriod;
+    public float rotSpeed;
+
+    private Vector3 _startPos;
+    private Vector3 _peak;
+    private Vector3 _base;
+    private float _time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position;
+        _peak = _startPos + Vector3.up * bobOffset;
+        _base = _startPos - Vector3.up * bobOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+        transform.position = Vector3.Lerp(
+            _peak,
+            _base,
+            (-Mathf.Cos(_time * bobPeriod) + 1) / 2
+            );
+
+        transform.rotation = Quaternion.Euler(0, _time * rotSpeed, 0);
+    }
+}
